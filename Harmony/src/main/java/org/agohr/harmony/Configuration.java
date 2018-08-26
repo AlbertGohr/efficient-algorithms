e014302faf68a141ac09633c1377f9b5ab045595
albert.gohr <albert.gohr@outlook.de>
2018-08-26 12:38:10 +0200

    introduced application.yml and Configuration.

---
+++ Harmony/src/main/java/org/agohr/harmony/Configuration.java
@@ -0,0 +1,11 @@
+package org.agohr.harmony;
+
+import lombok.Data;
+
+@Data
+public class Configuration {
+
+	private String filename;
+	private int seed;
+
+}
--- Harmony/src/main/java/org/agohr/harmony/Main.java
+++ Harmony/src/main/java/org/agohr/harmony/Main.java
@@ -1,5 +1,10 @@
 package org.agohr.harmony;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Random;
 
@@ -7,11 +12,15 @@ import org.agohr.harmony.composer.blackwalk.BlackWalk;
 import org.agohr.harmony.composer.Composer;
 import org.agohr.harmony.midi.MidiWriter;
 import org.agohr.harmony.notes.Channel;
+import org.yaml.snakeyaml.Yaml;
 
 public final class Main {
 
-    public static void main(final String[] args) {
-        Random random = new Random(0);
+    public static void main(final String[] args) throws IOException {
+
+		Configuration configuration = loadConfiguration();
+
+		Random random = new Random(configuration.getSeed());
         final Composer composer = new BlackWalk(random);
         final List<Channel> channels = composer.compose();
 
@@ -19,8 +28,15 @@ public final class Main {
 
         final MidiWriter mW = new MidiWriter();
         final String name = "demo";
-        final String copyright = "Albert J. Gohr";
-        final String strFilePath = "BlackWalk.mid";
-        mW.write(name, copyright, channels, strFilePath);
+        final String copyright = "harmony";
+        mW.write(name, copyright, channels, configuration.getFilename());
     }
+
+	private static Configuration loadConfiguration() throws IOException {
+		Yaml yaml = new Yaml();
+		Path path = Paths.get("application.yml");
+		try(InputStream in = Files.newInputStream(path) ) {
+			return yaml.loadAs( in, Configuration.class );
+		}
+	}
 }
